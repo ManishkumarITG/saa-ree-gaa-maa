2e9598c3f277da1f1977fbd467417be969d5f8b3
ManishKumarITG <manishkumar@itgeeks.com>
2025-11-06 17:39:12 +0530

    Login api init

--- app/services/API.js
+++ app/services/API.js
@@ -2,6 +2,7 @@ import axios from "axios";
 const API_BASE_URL = " https://love-lyrics-backend.vercel.app/api/v1";
 
 
+//Get Otp For Login
 async function getOtp(e_mail) {
     try {
         const response = await axios.post(`${API_BASE_URL}/auth/otp`, { e_mail });
@@ -11,7 +12,18 @@ async function getOtp(e_mail) {
     }
 }
 
+//Login  User
+async function LoginUser(Data) {
+    try {
+        const response = await axios.post(`${API_BASE_URL}/auth/login`, { Data});
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
+// Exports
 module.exports = {
     getOtp,
+    LoginUser
 };
